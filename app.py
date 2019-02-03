9df48772fb7bf94e16c0122ee4d24b6ce6fd7603
Nan-Chen Chen <nanchen.chen@gmail.com>
2019-02-02 21:52:58 -0800

    change labels

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'A'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'B'},
             ],
             'layout': {
                 'title': 'Dash Data Visualization'
